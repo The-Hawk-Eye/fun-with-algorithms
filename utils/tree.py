6b3ba480ce8aebdab3f0e14b0da512ec7fa73562
cacao-macao <pavel.z.tashev@protonmail.com>
2020-10-19 20:57:04 +0300

    added mutators

--- utils/tree.py
+++ utils/tree.py
@@ -2,20 +2,29 @@
 abstraction for a node of a tree. The position object supports
 the method:
     p.value(): Return the value of the node at Position p.
+    p.index(): Return the index of the node at Position p.
 
 The tree ADT supports the following accessor methods:
     T.root(): Return the Position of the root of the tree T.
-    T.is_root(p): Return True if Position p is the root of T.
     T.parent(p): Return the Position of the parent of Position p.
     T.num_children(p): Return the number of children of Position p.
     T.children(p): Generate an iteration of the children of Position p.
     T.depth(p): Return the depth of the node at Position p.
+    T.is_root(p): Return True if Position p is the root of T.
     T.is_leaf(p): Return True if Position p does not have any children.
-    len(T): Return the total number of nodes in the tree T.
     T.is_empty(): Return True if tree T does not contain any nodes.
+    T.positions(strategy): Generate an iteration of all positions of tree T
+                           using the given strategy for tree traversal.
+    iter(T): Generate an iteration of the values of the nodes of tree T.
+    len(T): Return the total number of nodes in the tree T.
+
+The tree ADT also supports the following mutator methods:
+    T.add_root(val): Place a node with the given value at the root of an empty tree.
+    T.add_child(p, val): Create a new child with the given value for node at Position p.
+    T.replace(p, val): Replace the value of the node at Position p with the new value.
 """
 
-from collections import deque
+from traversal_algorithms import breadth_first_traversal
 
 class Tree:
     #---------------- nested Node class ----------------------#
@@ -25,7 +34,7 @@ class Tree:
         def __init__(self, value, idx, depth, parent=None, children=None):
             """ Initialize a _Node instance.
             @param value: Value stored by the node.
-            @param idx (int): Integer used to uniquely define a Node object.
+            @param idx (int): Integer used to uniquely identify a Node object.
             @param depth (int): Depth of the node from the root of the tree.
             @param parent (Node): The parent of the node.
             @param children (List[Node]): A list of Nodes representing
@@ -71,12 +80,13 @@ class Tree:
             """
             return not (self == other)
 
-    #---------------- public accessors ----------------#
+    #---------------- tree initializer ----------------#
     def __init__(self):
         """ Initialize an empty tree. """
         self._root = None
         self._size = 0
 
+    #---------------- public accessors ----------------#
     def root(self):
         """ Return Position representing the root of the tree. """
         return self._make_position(self._root)
@@ -104,7 +114,7 @@ class Tree:
         """
         node = self._validate(p)
         for child in node._children:
-            yield child
+            yield self._make_position(child)
 
     def depth(self, p):
         """ Return the depth of the node at Position p.
@@ -113,21 +123,6 @@ class Tree:
         """
         node = self._validate(p)
         return node._depth
-
-    # def attach(self, p, T):
-    #     """ Attach the internal structure of tree T as a subtree at the node at position p.
-    #     Reset T to an empty tree.
-    #     @param p (Position): Position representing the node in the tree.
-    #     @param T (Tree): Tree object to be attached as a subtree of node at position p.
-    #     """
-        
-    #     node = self._validate(p)
-    #     T._root._parent = node
-    #     node._children.append(T._root)
-
-    def __len__(self):
-        """ Return the total number of nodes in the tree. """
-        return self._size
 
     def is_root(self, p):
         """ Return True if Position p represents the root of the tree. """
@@ -141,21 +136,59 @@ class Tree:
         """ Return True if the tree is empty. """
         return len(self) == 0
 
-    def print(self):
+    def positions(self, strategy=breadth_first_traversal):
+        """ Generate an iteration of all positions of the tree.
+        @param strategy (func): A function implementing the strategy for tree traversal.
+                                Default is breadth-first traversal.
+        @yield p (Position): Position representing the node in the tree.
         """
-        """
-        root = self.root()
-        frontier = deque()
-        frontier.append(root)
+        for p in strategy(self):
+            yield p
 
-        while len(frontier) > 0:    # frontier not empty
-            p = frontier.popleft()
-            if not isinstance(p, self.Position):
-                print(p)
-            else:
-                print(p.value())
-                frontier.append("node %d has %d children" % (p.value(), self.num_children(p)))
-                frontier.extend(self.children(p))
+    def __iter__(self):
+        """ Generate an iteration of all the values of the nodes of the tree. """
+        for p in self.positions():
+            yield p.value()
+
+    def __len__(self):
+        """ Return the total number of nodes in the tree. """
+        return self._size
+
+    #---------------- public mutators ----------------#
+    def add_root(self, val):
+        """ Place a node with the given value at the root of an empty tree.
+        Raise ValueError if the tree is not empty.
+        @param val: Value to be stored at the node.
+        @return root (Position): Return Position representing the root of the tree.
+        """
+        if self._root is not None:
+            raise ValueError("Root exists")
+        self._root = self._Node(val, idx=0, depth=0)
+        self._size = 1
+        return self._make_position(self._root)
+
+    def add_child(self, p, val):
+        """ Create a new child with the given value for node at Position p.
+        @param p (Position): Position representing the node in the tree.
+        @param val: Value to be stored at the child.
+        @return child (Position): Return Position representing the new child.
+        """
+        node = self._validate(p)
+        child = self._Node(val, idx=self._size, depth=self.depth(p) + 1, parent=node)
+        node._children.append(child)
+        self._size += 1
+        return self._make_position(child)
+
+    def replace(self, p, val):
+        """ Replace the value of the node at Position p with the new value.
+        @param p (Position): Position representing the node in the tree.
+        @param val: Value to be stored at the node.
+        @return old: Return the old value stored at the node.
+        """
+        node = self._validate(p)
+        old = node._value
+        node._value = val
+        return old
 
     #---- private methods - should not be invoked by the user ----#
     def _make_position(self, node):
@@ -172,38 +205,27 @@ class Tree:
             raise ValueError("p is no longer valid")
         return p._node
 
-    def _add_root(self, val):
-        """ Place a node with the given value at the root of an empty tree.
-        Raise ValueError if the tree is not empty.
-        @param val: Value to be stored at the node.
-        @return root (Position): Return Position representing the root of the tree.
-        """
-        if self._root is not None:
-            raise ValueError("Root exists")
-        self._root = self._Node(val, idx=0, depth=0)
-        self._size = 1
-        return self._make_position(self._root)
-
-    def _add_child(self, p, val):
-        """ Create a new child with the given value for node at Position p.
+    def _insert(self, p, val):
+        """ Insert a new node at Position p. Attach the subtree rooted at the existing
+        node as a child of the new node. Note that the depths of the nodes must be recomputed
+        after executing the method _insert.
         @param p (Position): Position representing the node in the tree.
-        @param val: Value to be stored at the child.
-        @return child: Return Position representing the new child.
+        @param val: Value to be stored at the new node.
+        @return new_p (Position): Return Position representing the new node.
         """
         node = self._validate(p)
-        child = self._Node(val, idx=self._size, depth=self.depth(p) + 1, parent=node)
-        child = self._make_position(child)
-        node._children.append(child)
+        new_node = self._Node(val, idx=self._size, depth=node._depth, parent=node._parent)
+        node._parent = new_node
+        new_node._children.append(node)
         self._size += 1
-        return child
+        return self._make_position(new_node)
 
-    def _replace(self, p, val):
-        """ Replace the value of the node at Position p with the new value.
-        @param p (Position): Position representing the node in the tree.
-        @param val: Value to be stored at the node.
-        @return old: Return the old value stored at the node.
-        """
-        node = self._validate(p)
-        old = node._value
-        node._value = val
-        return old
+    def _compute_depths(self):
+        for p in self.positions():
+            node = self._validate(p)
+            if p == self.root():
+                node._depth = 0
+            else:
+                parent = self.parent(p)
+                par_node = self._validate(parent)
+                node._depth = par_node._depth + 1
